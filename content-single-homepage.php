231eb1036ae97489efbb5829dca6ae05a2110146
Vahid Mohammadi <email@vmoh.ir>
2017-05-20 00:53:29 +0430

    adding post-format-icon

--- content-single-homepage.php
+++ content-single-homepage.php
@@ -1,4 +1,18 @@
 <a class="post-tumb-link" href="<?php echo get_the_permalink(); ?>">
+<?php if (is_admin()) {
+	$post_format = get_post_format() ? : 'standard';
+	switch ($post_format) {
+		case 'standard':
+			$icon = 'fa-pencil';
+			break;
+		
+		default:
+			# code...
+			break;
+	}
+	?>
+	<i class="post-format-icon fa <?php echo $icon; ?>" aria-hidden="true"></i>
+<?php } ?>
 	<div <?php post_class('home-latest-posts post-list-item clearfix'); ?> id="post-<?php echo get_the_ID(); ?>">
 		
 			<h2 class="title"><?php echo get_the_title(); ?></h2>

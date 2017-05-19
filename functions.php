c11cae1cfe62b3e1dcba049034fa7e42123b23ad
Vahid Mohammadi <email@vmoh.ir>
2017-05-20 01:51:35 +0430

    // Rename Aside post format to Quick Tip

--- functions.php
+++ functions.php
@@ -361,3 +361,31 @@ function ajax_login(){
 
     die();
 }
+
+// Rename Aside post format to Quick Tip
+function rename_post_formats( $safe_text ) {
+    if ( $safe_text == 'Aside' )
+        return 'Quick Tip';
+
+    return $safe_text;
+}
+add_filter( 'esc_html', 'rename_post_formats' );
+
+//rename Aside in posts list table
+function live_rename_formats() { 
+    global $current_screen;
+
+    if ( $current_screen->id == 'edit-post' ) { ?>
+        <script type="text/javascript">
+        jQuery('document').ready(function() {
+
+            jQuery("span.post-state-format").each(function() { 
+                if ( jQuery(this).text() == "Aside" )
+                    jQuery(this).text("Quick Tip");             
+            });
+
+        });      
+        </script>
+<?php }
+}
+add_action('admin_head', 'live_rename_formats');

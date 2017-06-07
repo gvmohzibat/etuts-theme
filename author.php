7555264289e646afbbc4693aa42c2fb7de7d7402
Vahid Mohammadi <email@vmoh.ir>
2017-06-07 23:51:05 +0430

    code beautify

--- author.php
+++ author.php
@@ -6,51 +6,51 @@ $author = get_user_by( 'id', $id );
 
 <section id="main" class="clearfix">
 
-    <div id="rightPad">			
+	<div id="rightPad">			
 <!-- authors posts -->
-    <?php if (have_posts()) : ?>
-    	<div id="lastPosts" class="user-page-section">
-    		<div id="last-posts-container" class="clearfix">
-	            <h1 class="section-title entry-title home-page-title"><?php _e('User\'s latest posts','etuts'); ?></h1>
-                <?php while (have_posts()) : the_post();
-                    get_template_part('content', 'single-homepage');
-                endwhile; ?>
-            </div>
-            <?php get_template_part('page','navigation'); ?>
-        </div>
-    <?php else :
-        get_template_part('content', 'noresults');
-    endif; ?>
+	<?php if (have_posts()) : ?>
+		<div id="lastPosts" class="user-page-section">
+			<div id="last-posts-container" class="clearfix">
+				<h1 class="section-title entry-title home-page-title"><?php _e('User\'s latest posts','etuts'); ?></h1>
+				<?php while (have_posts()) : the_post();
+					get_template_part('content', 'single-homepage');
+				endwhile; ?>
+			</div>
+			<?php get_template_part('page','navigation'); ?>
+		</div>
+	<?php else :
+		get_template_part('content', 'noresults');
+	endif; ?>
 
 <!-- authors stories -->
-    <?php // get user stories
-    query_posts([
-        'post_type'=>array('vmoh_user_stories'),
-        'author' => $id,
-    ]);
-    ?>
-    <?php if (have_posts()) : ?>
-        <div id="lastStories" class="user-page-section">
-            <div id="last-stories-container" class="clearfix">
-                <h1 class="section-title entry-title home-page-title"><?php _e('User Experiences','etuts'); ?></h1>
-                <?php while (have_posts()) : the_post();
-                    get_template_part('content', 'story-homepage');
-                endwhile; ?>
-            </div>
-            <?php get_template_part('page','navigation'); ?>
-        </div>
-    <?php else :
-        get_template_part('content', 'noresults');
-    endif; ?>
+	<?php // get user stories
+	query_posts([
+		'post_type'=>array('vmoh_user_stories'),
+		'author' => $id,
+	]);
+	?>
+	<?php if (have_posts()) : ?>
+		<div id="lastStories" class="user-page-section">
+			<div id="last-stories-container" class="clearfix">
+				<h1 class="section-title entry-title home-page-title"><?php _e('User Experiences','etuts'); ?></h1>
+				<?php while (have_posts()) : the_post();
+					get_template_part('content', 'story-homepage');
+				endwhile; ?>
+			</div>
+			<?php get_template_part('page','navigation'); ?>
+		</div>
+	<?php else :
+		get_template_part('content', 'noresults');
+	endif; ?>
 
-    </div>
-        
+	</div>
+		
 
-    <div id="leftPad">
-        <div>
-            <?php get_sidebar('user'); ?>
-        </div>
-    </div>
+	<div id="leftPad">
+		<div>
+			<?php get_sidebar('user'); ?>
+		</div>
+	</div>
 
 </section>
 <?php get_footer(); ?>

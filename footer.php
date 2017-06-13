5130553c79ddf614f3410a9e4d630294839d70c4
Vahid Mohammadi <email@vmoh.ir>
2017-06-14 00:02:19 +0430

    extra-footer

--- footer.php
+++ footer.php
@@ -1,7 +1,14 @@
 <?php get_sidebar('overlay-bar'); ?>
 <div id="footer">
 	<?php get_sidebar('footer-widget'); ?>
-	<div id="copyright">&copy; 2016 Hadi Safari &amp; Vahid Mohammadi</div>
+	<div id="extra-footer" class="clearfix">
+		<p id="copyright">&copy; 2016 Hadi Safari &amp; Vahid Mohammadi</p>
+		<ul id="footer-links">
+			<li><a href="<?php echo get_site_url(); ?>/contact" title="تماس با ما"><i class="fa fa-phone" aria-hidden="true"></i> تماس با ما</a></li>
+			<li><a href="http://t.me/etuts" title="کانال تلگرام"><i class="fa fa-bullhorn" aria-hidden="true"></i> <span style="direction: ltr; display: inline-block;">@etuts</span></a></li>
+			<li><a href="http://t.me/etutsbot" title="ربات تلگرام"><i class="fa fa-paper-plane-o" aria-hidden="true"></i> <span style="direction: ltr; display: inline-block;">@etutsbot</span></a></li>
+		</ul>
+	</div>
 </div>
 <?php wp_footer(); ?>
 <script>
